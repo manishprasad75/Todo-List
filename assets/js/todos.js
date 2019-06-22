23c2296339b1e751b10599dcc38edcb1b62c368b
manishprasad75 <iamoneofmyownkind@gmail.com>
2019-06-22 17:50:59 +0530

    first Commit

---
+++ assets/js/todos.js
@@ -0,0 +1,32 @@
+//Check Off Specigic Todos By Clicking
+$('ul').on('click','li',function(){
+	$(this).toggleClass('completed');
+})
+
+//Click on X to delete Todo
+
+$('ul').on("click","li span",function(event){
+	// console.log('click on span');
+
+	//TO STOP BUBBLING
+	event.stopPropagation();
+	$(this).parent().fadeOut(500,function(){
+		$(this).remove();
+	});
+
+});
+
+$("input[type='text'").keypress(function(event){
+	if(event.which == '13')
+	{
+		let todoText = $(this).val();
+		$(this).val("");
+
+		//create a new li and add to ul
+		$('ul').append(`<li><span><i class="fa fa-trash"></i></span> ${todoText} </li>`)
+	}
+});
+
+$('.fa-plus').click(function(){
+	$("input[type='text'").fadeToggle();
+});
